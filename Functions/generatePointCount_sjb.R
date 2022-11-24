8d08c42aad987cc56beef242f2dca634adb5960f
Simon Bonner <sbonner6@uwo.ca>
2022-11-24 10:31:37 -0500

    Changed pc_data to pcData to match camel case.

--- Functions/generatePointCount_sjb.R
+++ Functions/generatePointCount_sjb.R
@@ -1,14 +1,16 @@
 generateDetects <- function(Ni,p_1m,maxMinute, seed = NULL){
   
   ## Initialize data frame with location, individual, and occasion
-  pc_data <- tibble(locationID = 1:length(Ni)) %>%
+  pcData <- tibble(locationID = 1:length(Ni)) %>%
     group_by(locationID) %>%
     summarize(individual = 1:Ni[locationID], .groups = "drop") %>%
     crossing(Minute = 1:maxMinute)
   
   ## Simulate detections
-  pc_data <- pc_data %>%
+  pcData <- pcData %>%
     mutate(Detect = rbinom(n(), 1, p_1m))
   
-  pc_data
+  pcData
 }
+
+detectsToCapHist <- function(pcData, keepZeros)
