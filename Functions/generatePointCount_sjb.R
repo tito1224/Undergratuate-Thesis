90efc617611d57db81ff9468fe3d8273b2880e12
Simon Bonner <sbonner6@uwo.ca>
2022-11-24 10:29:52 -0500

    Added function to simulate detections per individual.

--- Functions/generatePointCount_sjb.R
+++ Functions/generatePointCount_sjb.R
@@ -1,4 +1,14 @@
 generatePointCount <- function(Ni,p_1m,maxMinute, seed = NULL){
   
-  ## Initialize data frame
+  ## Initialize data frame with location, individual, and occasion
+  pc_data <- tibble(locationID = 1:length(Ni)) %>%
+    group_by(locationID) %>%
+    summarize(individual = 1:Ni[locationID], .groups = "drop") %>%
+    crossing(Minute = 1:maxMinute)
   
+  ## Simulate detections
+  pc_data <- pc_data %>%
+    mutate(Detect = rbinom(n(), 1, p_1m))
+  
+  pc_data
+}
