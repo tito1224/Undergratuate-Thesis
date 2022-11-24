49503cee5ddd9674c1a4fc7ae5023250f2d7b90b
Simon Bonner <sbonner6@uwo.ca>
2022-11-24 10:41:28 -0500

    Added functions to pivot data and to simulate movmements.

--- Functions/generatePointCount_sjb.R
+++ Functions/generatePointCount_sjb.R
@@ -13,4 +13,34 @@ generateDetects <- function(Ni,p_1m,maxMinute, seed = NULL){
   pcData
 }
 
-detectsToCapHist <- function(pcData, keepZeros)
+detectsToCapHist <- function(pcData, keepZeros = FALSE){
+  
+  ## Remove individuals that were never detected
+  if(!keepZeros){
+    
+    ## Compute number of detections per individual
+    pcData <- pcData %>%
+      group_by(locationID,individual) %>%
+      mutate(Detects = sum(Detect)) %>%
+      filter(Detects > 0) %>%
+      select(-Detects)
+  }
+  
+  ## Pivot data
+  chData <- pcData %>%
+    mutate(Minute = paste0("Minute",Minute)) %>%
+    pivot_wider(names_from = Minute,
+                values_from = Detect)
+  
+  chData
+}
+
+generateMovement <- function(pcData, alpha){
+  
+  ## Simulate movements
+  pcData <- pcData %>%
+    mutate(Move = (Minute > 1) * rbinom(n(), 1, alpha))
+  
+  pcData
+}
+
