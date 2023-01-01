b8c1165f2a6ddaac18a933280db7991502724afa
tito1224 <64660812+tito1224@users.noreply.github.com>
2023-01-01 18:49:16 -0500

    Create dataframe to store formulas for 12 MLE models

--- runSimulation.R
+++ runSimulation.R
@@ -1,23 +1,58 @@
 library(tidyverse)
 library(RMark)
+library(stringi)
 source("./Functions/generatePointCount.R")
 
 # start with helper functions
 
 
-## create a dataframe to represent all model combinations
-## does FullHet include the three way interactions whereas the HetClosed is the 12 models described in Pledger's paper? - nvm 
-## actually I believe if we need to model individual heterogeneity (ie use mixture argument) then we use either HetClosed or FullHet
-## use FullHet in order to have additive values that include heterogeneity? on page 23 of the program mark appendix, looks like they use the FullHet model  
-## useC means detection probability after first capture is different (takes into account trap happy or trap shy effect)
-## useTime means detection probability varies across time intervals --> do we assume random variation? 
-## useMixture means heterogeneity in detection probability across individuals 
-dataVariation = function(useC=FALSE,useTime=FALSE,useMixture=FALSE){
+## create a dataframe to represent all model combinations - doing it this way so if we need to make a change to the formula, it only needs to be made once in this section
+## I believe if we need to model individual heterogeneity (ie use mixture argument) then we use either HetClosed or FullHet
+## HetClosed does not include c as a parameter whereas FullHet includes c (refer to https://github.com/jlaake/RMark/blob/master/RMark/inst/MarkModels.pdf)
+## also it seems we can only have two groups for the mixture model?
+## useC means detection probability after first capture is different (takes into account trap happy or trap shy effect; ie behaviour)
+## useTime means detection probability varies across time intervals --> assume random variation? 
+## useMixture means heterogeneity in detection probability across individuals (so if we assume A groups, each individual has a probability of belonging to each group. I think rMark uses 2 groups)
+generateFormula = function(){
+  # create combinations
+  lstC = c("c","NA")
+  lstTime = c("time","NA")
+  lstMixture = c("mixture","NA")
+  lstOperation =c("+","*") # whether it's additive or we include interaction
+  dfCombinations = expand.grid(lstC,lstTime,lstMixture,lstOperation)
+  colnames(dfCombinations) = c("C","Time","Mixture","Operation")
+  dfCombinations = mutate(dfCombinations, formula = NA)
   
+  # add what the formula should be 
+  ## code below creates formulas for Pledger's 12 models (not including partially interactive models between M_t*b*h and M_t+b+h)
+  
+  # boolean to indicate whether row is an additive model or interactive
+  bAddition = which(dfCombinations$Operation=="+")
+  bInteraction = which(dfCombinations$Operation=="*")
+  dfCombinations[bAddition,"formula"] = paste(dfCombinations[bAddition,"C"],dfCombinations[bAddition,"Time"],dfCombinations[bAddition,"Mixture"],sep = "+")
+  dfCombinations[bInteraction,"formula"] = paste(dfCombinations[bInteraction,"C"],dfCombinations[bInteraction,"Time"],dfCombinations[bInteraction,"Mixture"],sep = "*")
+  
+  # do some cleaning of the string -> remove whitespaces and instances where the formula starts/ends with an operator
+  lstRemove = c("NA+","+NA"," ","NA*","*NA")
+  dfCombinations$formula = stri_replace_all_fixed(dfCombinations$formula,
+                                                  pattern = lstRemove, 
+                                                  replacement = rep("",length(lstRemove)),
+                                                  vectorize_all = FALSE)
+  
+ dfCombinations$formula = paste("~",dfCombinations$formula,sep="")
+ #dfCombinations = dfCombinations[!duplicated(dfCombinations$formula),]
+ dfCombinations[dfCombinations$formula=="~NA","formula"] = "~1"
+ 
+ # add another column to indicate what the model type should be 
+ dfCombinations$model = ifelse(dfCombinations$Mixture=="NA","Closed", ifelse(dfCombinations$Mixture!="NA"& dfCombinations$C == "NA","HetClosed","FullHet"))
+ 
+ # revert values in C, Time and Mixture columns to boolean for use in later code
+ dfCombinations[,1:3] = ifelse(dfCombinations[,1:3]=="NA",FALSE,TRUE)
+ return(dfCombinations)
 }
 
 ## assuming we have the data, fit the model, with necessary specifications
-## useC means detection probability after first capture is different (takes into account)
-fitModel = function(ch,useC=FALSE,useTime=FALSE,useMixture=FALSE){
+## use the parameters to determine which formula and model to use
+fitModel = function(ch,bC=FALSE,bTime=FALSE,bMixture=FALSE,bAdditive=TRUE){
   
 }
