c1221012be82d8f520822a8a0df35d4327637bce
tito1224 <64660812+tito1224@users.noreply.github.com>
2023-01-26 16:17:43 -0500

    Try to fix outfile message

--- Functions/runSimulation.R
+++ Functions/runSimulation.R
@@ -18,10 +18,10 @@ id = as.integer(args[1]) # what is this??
 
 ## list parameters and make a dataframe out of it as inputs
 nRuns = 2
-lstNi = c(20,50)
+lstNi = c(20)
 lstP = c(0.1,0.2)
-lstAlpha =c(0.1,0.3)
-lstMaxMin = c(5,10)
+lstAlpha =c(0.1)
+lstMaxMin = c(5)
 lstFormula = c("~1")
 lstMixtures = c(1)
 lstSeed = c("NULL")
@@ -30,12 +30,21 @@ strModel = "Closed"
 params = expand.grid(nRuns,lstNi,lstP, lstAlpha, lstMaxMin,lstFormula,lstMixtures,lstSeed,strModel)
 colnames(params) = c("nRuns","lstNi","lstP","lstAlpha","lstMaxMin","lstFormula","lstMixtures","lstSeed","strModel")
 params$lstFormula = as.character(params$lstFormula) # for some reason this column turns into a factor variable?
+
 # params = mutate(params, Scenario = row_number()) %>%
 #   crossing(Rep = 1:nRuns)
 
 ## Run simulation with parameters from id row of parameter matrix
-results <- simulation_wrapper(params[id,])
+results = simulation_wrapper(params[id,])
 
+
+# run the simulation
+# results = calculateStatistics(nRuns = nRuns, lstNi = lstNi, lstP = lstP, 
+#                               lstAlpha = lstAlpha, lstMaxMin = lstMaxMin,
+#                               lstFormula=lstFormula,lstMixtures=lstMixtures,
+#                               seed=lstSeed,strModel=strModel)
+
+# return results
 ## Save that number
-outfile <- paste0("Output/output_",params$Scenario[id],"_",params$Rep[id],".rds")
-saveRDS(r,outfile)
+outfile = paste0("Output/output_",id,"_",".rds")
+saveRDS(results,outfile)
